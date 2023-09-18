1136bf2634b52c0998c650ff79f50877cdd8c95e
Abdulaziz Alsayed Ahmad <abdulaziz.alsayed.ahmad@preciofishbone.se>
2023-09-18 15:46:54 +0200

    last update

--- main.js
+++ main.js
@@ -13,22 +13,12 @@ let img = document.getElementById("img");
 let reset = document.querySelector("span");
 let imgBox = document.querySelector(".img-box");
 
-// Detect touch support
-let isTouchDevice = 'ontouchstart' in window || navigator.msMaxTouchPoints;
-
-
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
-// Hide elements on page load
-window.onload = function () {
-  download.style.display = "none";
-  reset.style.display = "none";
-  imgBox.style.display = "none";
-};
-
-// Reset filter values
+//when u upload a new img
 function resetValue() {
+ img.filter = "none";
   saturate.value = "100";
   contrast.value = "100";
   brightness.value = "100";
@@ -36,10 +26,17 @@ function resetValue() {
   grayscale.value = "0";
   blur.value = "0";
   hue_rotate.value = "0";
-  applyFilters(); // Apply default filters immediately
+  
 }
 
-// Upload image
+//hide when u don't have a img
+window.onload = function () {
+  download.style.display = "none";
+  reset.style.display = "none";
+  imgBox.style.display = "none";
+};
+
+//show when u have a img
 upload.onchange = function () {
   resetValue();
   download.style.display = "block";
@@ -48,59 +45,36 @@ upload.onchange = function () {
   let file = new FileReader();
   file.readAsDataURL(upload.files[0]);
   file.onload = function () {
-    img.src = file.result;
-    img.onload = function () {
-      canvas.width = img.width;
-      canvas.height = img.height;
-      context.drawImage(img, 0, 0, canvas.width, canvas.height);
-      applyFilters(); // Apply filters when a new image is loaded
-    };
+  img.src = file.result;
+  };
+
+  //To downlod the image with the filters,
+  //we need to create a copy of the img on the canvas
+  img.onload = function () {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    context.drawImage(img, 0, 0, canvas.width, canvas.height);
+   img.style.display = "none";
   };
 };
 
-// Apply filters
-function applyFilters() {
-  context.filter = `
-    saturate(${saturate.value}%)
-    contrast(${contrast.value}%)
-    brightness(${brightness.value}%)
-    sepia(${sepia.value}%)
-    grayscale(${grayscale.value})
-    blur(${blur.value}px)
-    hue-rotate(${hue_rotate.value}deg)
-  `;
-  context.drawImage(img, 0, 0, canvas.width, canvas.height);
-}
-
-// Add input event listeners for filter adjustments
 let filters = document.querySelectorAll("ul li input");
 filters.forEach((filter) => {
-  filter.addEventListener("input", applyFilters);
+  filter.addEventListener("input", function () {
+    context.filter = `
+        saturate(${saturate.value}%)
+        contrast(${contrast.value}%)
+        brightness(${brightness.value}%)
+        sepia(${sepia.value}%)
+        grayscale(${grayscale.value})
+        blur(${blur.value}px)
+        hue-rotate(${hue_rotate.value}deg)        
+        `;
+    context.drawImage(img, 0, 0, canvas.width, canvas.height);
+  });
 });
 
-// Download function
+//downlod function
 download.onclick = function () {
   download.href = canvas.toDataURL();
 };
-
-
-// Function to handle input events (used for both mouse and touch events)
-function handleInput() {
-  applyFilters();
-}
-
-// Add event listeners for both input and touch events
-if (isTouchDevice) {
-  filters.forEach((filter) => {
-    filter.addEventListener("touchmove", handleInput);
-  });
-} else {
-  filters.forEach((filter) => {
-    filter.addEventListener("input", handleInput);
-  });
-}
-// Add input event listeners for filter adjustments (both mouse and touch events)
-filters.forEach((filter) => {
-  filter.addEventListener("input", applyFilters);
-  filter.addEventListener("touchmove", applyFilters);
-});
